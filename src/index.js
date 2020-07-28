67684c51801f7eb1dd4973060d4561a7aee26ca0
John Fredy <john_fred_y26@hotmail.com>
2020-07-28 14:19:18 -0500

    Implementando JSX

--- src/index.js
+++ src/index.js
@@ -7,10 +7,15 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const element = <h1>Hello, Platzi Badges!</h1>;  // esto sea hace con JSX
-
+// const name = 'Angela'
+// const element = <h1>Hello, Platzi Badges!</h1>;  // esto sea hace con JSX
+// const jsx = <h1>Hola soy , {name}</h1>
+const jsx = (<div>
+    <h1> Hola, soy John
+    </h1>
+    <p>Soy ingeniero Frontend</p>
+    </div>)
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+ReactDOM.render(jsx, container);
