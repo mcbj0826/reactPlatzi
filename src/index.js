4a801b281f44f565e719637fa1a9643d3d121f8d
John Fredy <john_fred_y26@hotmail.com>
2020-07-30 14:18:53 -0500

    Listas de componentes.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 import './global.css'
 import BadgeNew from './pages/BadgeNew'
+import Badges from './pages/Badges'
 
 
 const container = document.getElementById('app')
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(<BadgeNew />, container)
+ReactDOM.render(<Badges />, container)
