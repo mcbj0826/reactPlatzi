331e8876d1c67a4765b1db00c91fab899a7fdaf7
John Fredy <john_fred_y26@hotmail.com>
2020-07-29 20:29:17 -0500

    Imprementando Props.

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,26 +1,29 @@
 import React from 'react'
+import "./styles/Badge.css"
 import confLogo from '../images/badge-header.svg'
 
 
 class Badge extends React.Component {
     render() {
-        return <div>
-            <div>
+    
+        const firstName = 'John'
+        const lastName = 'Suarez'
+        return <div className="Badge">
+            <div className="Badge__header">
                 <img src={confLogo} alt =" Logo de la conferencia "></img>
             </div>
 
-            <div>
-                <img src= "" alt = "Imágen Avatar"></img>
-                <h1>John <br/> Suárez</h1>
-
-                
+            <div className="Badge__section-name">
+                <img  className= "Badge__avatar" src= {this.props.avatarUrl} alt = "Imágen Avatar"></img>
+                <h1>{this.props.firstName} <br/> {this.props.lastName}</h1>
             </div>
 
-            <div>
-                <p>Frontend Engineer</p>
-                <p>@johnfredy26</p>
-
+            <div className="Badge__section-info">
+                <h3>{this.props.jobTitle}</h3>
+                <div>{this.props.twitter}</div>
             </div>
+
+            <div className="Badge__footer">#Platziconf</div>
             
         </div>    
     }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.css'
+import './global.css'
 import Badge from './components/Badge'
+
 
 const container = document.getElementById('app')
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(<Badge />, container)
+ReactDOM.render(<Badge firstName="John" lastName="Suárez" jobTitle="Frontend Engineer" twitter="@fredy082" avatarUrl="acá va la imágen del avatar mi foto" />, container)
