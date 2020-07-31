ed903ab6db9d4de509511feb4846d635ee58bf09
John Fredy <john_fred_y26@hotmail.com>
2020-07-30 20:25:42 -0500

    Solicitando Datos GET.

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 class BadgesList extends React.Component {
 
     render() {
+        if (this.props.badges.length === 0) {
+            return (
+                <div>
+                    <h3>No badges were found</h3>
+                    <Link className="btn btn-primary" to="/badges/new">Create new badge</Link>
+
+                </div>
+            )
+        }
         return (
             <ul className="list-untyled">
                 {this.props.badges.map((badge)=> {
